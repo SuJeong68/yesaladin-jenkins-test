ce1f76362bcfe9ccaeade2b5995fa6c1c415a410
SuJeong68 <tidh1640@naver.com>
2023-01-02 17:06:51 +0900

    Initial Commit

---
+++ src/main/java/shop/yesaladin/yesaladinjenkinstest/YesaladinJenkinsTestApplication.java
@@ -0,0 +1,13 @@
+package shop.yesaladin.yesaladinjenkinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesaladinJenkinsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YesaladinJenkinsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/shop/yesaladin/yesaladinjenkinstest/YesaladinJenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.yesaladin.yesaladinjenkinstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YesaladinJenkinsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
