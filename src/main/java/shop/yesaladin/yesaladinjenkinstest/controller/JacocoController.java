ebd5b06c58dba9ed593a0c146304c60ebd36c07d
SuJeong68 <tidh1640@naver.com>
2023-01-02 19:43:29 +0900

    Fix: git 수정

--- src/main/java/shop/yesaladin/yesaladinjenkinstest/controller/JacocoController.java
+++ src/main/java/shop/yesaladin/yesaladinjenkinstest/controller/JacocoController.java
@@ -1,2 +1,19 @@
-package shop.yesaladin.yesaladinjenkinstest.controller;public class JacocoController {
+package shop.yesaladin.yesaladinjenkinstest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JacocoController {
+
+    @GetMapping("/test")
+    public String test(@RequestParam int n) {
+
+        if (n >= 0) {
+            return "hello";
+        } else {
+            return "world";
+        }
+    }
 }
